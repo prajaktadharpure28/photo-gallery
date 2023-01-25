ab35f03972b0854136254a08e02c0f245fa2c52a
prajaktadharpure28 <prajaktadharpure28@gmail.com>
2023-01-25 17:37:13 +0530

    feat: connect mongodb url

---
+++ server/index.js
@@ -0,0 +1,18 @@
+import express from 'express';
+import mongoose from 'mongoose';
+import dotenv from 'dotenv';
+dotenv.config();
+
+
+const app = express();
+app.use(express.json());
+
+const PORT = process.env.PORT || 5000;
+
+mongoose.connect(process.env.MONGODB_URL, ()=>{
+    console.log('Connected to MongoDB');
+})
+
+app.listen(PORT, () => {
+    console.log(`Server is running on port ${PORT}`);
+});
